3060af47646a80e50b46d7067cd81a3a8526c58e
liqi <940608848@qq.com>
2017-07-20 17:14:25 +0800

    接入腾讯TLS的用户签名sig工具，在lib\tencent目录下，公私钥和js代码

---
+++ src/lib/tencent/usersig.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017/7/20.
+ */
+'use strict';
+
+/**
+ * 根据腾讯im的文档，生成用户签名sig的方法
+ * 私钥和公钥是腾讯提供的，方法也是从官方文档中copy下来的
+ * 根据实验结果发现，每次加密的sig不同，这跟sig的有效期限也是有关的
+ * expire_after可以指定sig的有效期限
+ * 每次执行都相当于重新生成一个签名sig
+ * 机制：后台不保存sig，由前端自己保存，前端请求login时再重新生成一次
+ */
+
+var sig = require('./TLSAPI');
+var config = {
+    "sdk_appid": 1400036325,
+    "expire_after": 180 * 24 * 3600,
+    "private_key": "private_key",
+    "public_key": "public_key"
+}
+
+var sig = new sig.Sig(config);
+console.log(sig.genSig("1001"));
